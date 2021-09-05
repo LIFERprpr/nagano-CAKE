beb6da19e3ade922b316169e2025fb32e9b2e755
EC2 Default User <ec2-user@ip-172-31-82-144.ec2.internal>
2021-09-05 11:01:25 +0000

    [Finish]顧客側のaboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,36 @@
 Rails.application.routes.draw do
-  
+
   namespace :admins do
     resources :items, except: [:destroy]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
-    
-    
+
+
     get 'homes/top'
   end
   # adminsフォルダのhomes#topをルートに指定し、URLを/adminに変更
   namespace :admins do
     root 'homes#top'
   end
-  
+
   devise_for :admins, controllers: {
     sessions:       'admins/sessions',
     passwords:      'admins/passwords',
     registrations:  'admins/registrations'
   }
-  
+
   scope module: :public do
-    
+
   end
-  
+
   root to: 'homes#top'
-  
+  get '/about' => 'homes#about'
   devise_for :customers, controllers: {
     sessions:       'customers/sessions',
     passwords:      'customers/passwords',
     registrations:  'customers/registrations'
   }
-  
-  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
