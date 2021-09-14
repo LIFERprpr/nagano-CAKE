317db8140adc4422ad34a63f0a4b9b477bbb8cdc
EC2 Default User <ec2-user@ip-172-31-82-144.ec2.internal>
2021-09-14 08:59:37 +0000

    [Add]顧客のショッピングカート画面の作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = Cartitem.all
+    @cart_items = current_customer.cart_items
   end
-  
+
   def create
+    @cart_item = current_customer.cart_items.new(params_cart_item)
+    @cart_item.save
+    redirect_to customers_cart_items_path
   end
-  
+
   def update
   end
-  
+
   def destroy
+  end
+
+  private
+
+  def params_cart_item
+    params.require(:cart_item).permit(:amount, :item_id)
   end
 end
